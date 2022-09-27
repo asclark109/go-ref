9e72fb8f0f06d0b0d21638fd71b38e8f692b59f6
asclark109 <asclark109@gmail.com>
2022-09-27 15:23:10 -0500

    init commit: try basic programs

---
+++ lcogolangbootcamp/01basics/01variables.go
@@ -0,0 +1,40 @@
+package main
+
+// import "fmt"
+
+// // func main() {
+// // 	var batman string = "I am batman"
+// // 	fmt.Println(batman)
+
+// // 	var superman string
+// // 	superman = "I am superman"
+// // 	fmt.Println(superman)
+
+// // 	thor := "I am thor" // := only usable within function assignment + type inference
+// // 	fmt.Println(thor)
+
+// // 	// thorRating := 3 // creates int
+// // 	// thorRating := 3. // creates float64
+// // 	// fmt.Printf("%v, %T", thorRating, thorRating) // value, type
+
+// // 	var Ironman, CatAmerica string = "I am ironman", "I am capt America"
+// // 	fmt.Println(Ironman, CatAmerica)
+
+// // 	var defaultValue int // 0
+// // 	// var defaultValue string // ""
+// // 	fmt.Println(defaultValue)
+
+// // 	// declare multiple vars
+// // 	var (
+// // 		spiderman = "I am spiderman"
+// // 		age       = 18
+// // 		powers    = "web slings, spidy sense"
+// // 		antman    = "I am antman"
+// // 	)
+// // 	fmt.Println(spiderman, age, powers, antman)
+
+// // 	fmt.Println()
+// // 	fmt.Printf("%v, %T", thor, thor) // value, type
+// // 	fmt.Printf("%v, %T", age, age)   // value, type
+
+// // }
---
+++ lcogolangbootcamp/01basics/02takeinput.go
@@ -0,0 +1,14 @@
+package main
+
+// to take input from user; can create a reader that can read from standard input
+// string converter will need for project
+
+func main() {
+	// var myString string
+	// fmt.Scanln(&myString) // & means store result in variable myString
+	// // note, weirdness happens if you enter a space eg Austin Clark
+	// // 1 2 3 4
+	// // 1
+	// fmt.Println(myString)
+
+}
